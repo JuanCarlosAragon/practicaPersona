9c6ff9bb88fdd85600605c1445368e2b7dd4465a
Juan Carlos Aragón <juankar.md@gmail.com>
2014-10-27 08:59:32 +0100

    Clase persona que pide el parametro edad y con metodo getEdad que te devuelve el parametro

---
+++ Persona.java
@@ -0,0 +1,15 @@
+public class Persona 
+{
+    //Declaramos los atributos
+    private int edad;
+    
+    public Persona(int age)//creamos el constructor pidiendo el parametro age
+    {
+        edad = age;
+    }
+    
+    public int getEdad()//creamos el método que nos devuelve la edad
+    {
+        return edad;
+    }
+}
